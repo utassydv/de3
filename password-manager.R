6356e5e6880c7e5f588cc08a89db4cdcad6f2276
David Utassy <utassydv@gmail.com>
2020-11-22 14:31:51 +0100

    add finished password-manager

--- password-manager.R
+++ password-manager.R
@@ -12,8 +12,7 @@ init.table <- function(){
   #' Create and return empty dataframe with these columns:
   #' user.name : character
   #' password: character
-  df <- data.frame(user.name=character(),
-                   password=character()) 
+  df <- data.frame('user.name'=character(), 'password'=character()) 
   return(df)
 }
 
@@ -24,16 +23,30 @@ add.or.update.user <- function(df, user.name, password){
   #' instead of creating a duplicate
   #' 
   #' return with the updated DataFrame
-  df = rbind(df, c(user.name, encrypt(password)))
+  
+  already_exists = FALSE
+  
+  for( i in seq_along(df$user.name)){
+    if (df$user.name[i] == user.name ){
+      df$password[i] <- encrypt(password)
+      already_exists = TRUE
+    }
+  }
+  
+  if (!already_exists){
+   df = rbind(df,list('user.name'=user.name, 'password'=encrypt(password)))
+  }
   
   return(df)
 }
 
 authenticate.user <- function (df, user.name, password){
-  #' Check if the user with the specified `user.name` and `password`
-  #' exists in `df`
-  #' Return TRUE/FALSE respectively.
-  return(any(df==user.name) && any(df==encrypt(password)))
+  for(i in seq_along(df$user.name)){
+    if(df$user.name[i] == user.name & df$password[i] == encrypt(password)){
+      return(TRUE)
+    } 
+  }
+  return(FALSE)
 }
 
 # Example execution
@@ -48,3 +61,4 @@ print(nrow(user.df[user.df$password == "example_password_2",]) == 0)
 print(nrow(user.df[user.df$user.name == "example_user",]) == 1)
 print(authenticate.user(user.df, "example_user", "example_password_2"))
 print(!authenticate.user(user.df, "example_user", "example_password"))
+
